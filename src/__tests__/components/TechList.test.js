fab3af3f73fe4affd4f405ad4dec5a00d7d06177
Igor Souza <igorsouza.dev@gmail.com>
2019-10-19 08:43:42 -0300

    Added techlist component test

---
+++ src/__tests__/components/TechList.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TechList from "~/components/TechList";
+
+describe("TechList component", () => {
+  it("should be able to add new tech", () => {
+    const { getByText, getByTestId, debug } = render(<TechList />);
+    debug();
+
+    fireEvent.click(getByText("Add"));
+
+    debug();
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+  });
+});
