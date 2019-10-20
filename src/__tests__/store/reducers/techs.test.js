c0676474ef7d9b5ae0f7e36ec4cd930888ed6cd3
Igor Souza <igorsouza.dev@gmail.com>
2019-10-20 14:10:16 -0300

    Mocking api. Testing saga. Added coverage

--- src/__tests__/store/reducers/techs.test.js
+++ src/__tests__/store/reducers/techs.test.js
@@ -2,6 +2,11 @@ import reducer, { INITIAL_STATE } from "~/store/modules/techs/reducer";
 import * as Techs from "~/store/modules/techs/actions";
 
 describe("Techs reducers", () => {
+  it("DEFAULT", () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
   it("ADD_TECH", () => {
     const state = reducer(INITIAL_STATE, Techs.addTech("Node.js"));
 
--- src/store/modules/techs/actions.js
+++ src/store/modules/techs/actions.js
@@ -6,3 +6,16 @@ export function addTech(tech) {
     }
   };
 }
+
+export function getTechsSuccess(data) {
+  return {
+    type: "GET_TECHS_SUCCESS",
+    payload: { data }
+  };
+}
+
+export function getTechsFailure() {
+  return {
+    type: "GET_TECHS_FAILURE"
+  };
+}
